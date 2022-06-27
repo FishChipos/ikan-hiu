cf9829cf04c4de79374b72d622a6e9410e8fc90f
Carlsson Khovis <63272281+FishChipos@users.noreply.github.com>
2022-06-27 12:41:56 +0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,69 +1,93 @@
+pins.set_pull(DigitalPin.P20, PinPullMode.PULL_UP)
+
 # Utility
 # Turn left 90 degrees
 def turn_left():
     counter = 0
 
-    sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 100)
-    sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 100)
-
     while True:
-        if (counter >= 2 and ir1 == 1):    
-            sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 0)
-            sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 0)
+        basic.show_number(counter)
+        sensors.dd_mmotor(gb1_direction, 1, gb1_speed, turn_speed)
+        sensors.dd_mmotor(gb2_direction, 1, gb2_speed, turn_speed)
+
+        if (counter >= 2 and pins.digital_read_pin(ir1) == 0):    
+            sensors.dd_mmotor(gb1_direction, 1, gb1_speed, 0)
+            sensors.dd_mmotor(gb2_direction, 1, gb2_speed, 0)
             break
 
-        if (counter == 0 and ir1 == 0):
+        if (counter == 0 and pins.digital_read_pin(ir1) == 0):
             counter = 1
 
-        elif (counter == 1 and ir1 == 1):
+        if (counter == 1 and pins.digital_read_pin(ir1) == 1):
             counter = 2
 
 # Turn right 90 degrees
 def turn_right():
     counter = 0
 
-    sensors.dd_mmotor(gb1_direction, 1, gb1_speed, 100)
-    sensors.dd_mmotor(gb2_direction, 1, gb2_speed, 100)
-
     while True:
-        if (counter >= 2 and ir2 == 1):
-            sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 0)
-            sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 0)
+        basic.show_number(counter)
+        sensors.dd_mmotor(gb1_direction, 0, gb1_speed, turn_speed)
+        sensors.dd_mmotor(gb2_direction, 0, gb2_speed, turn_speed)
+
+        if (counter >= 2 and pins.digital_read_pin(ir2) == 0):
+            sensors.dd_mmotor(gb1_direction, 1, gb1_speed, 0)
+            sensors.dd_mmotor(gb2_direction, 1, gb2_speed, 0)
             break
 
-        if (counter == 0 and ir2 == 0):
+        if (counter == 0 and pins.digital_read_pin(ir2) == 0):
             counter = 1
 
-        elif (counter == 1 and ir2 == 1):
+        if (counter == 1 and pins.digital_read_pin(ir2) == 1):
             counter = 2
 
 # Adjust angle so that the car is straight
 def adjust_left():
-    while (ir1 == 0 and ir2 == 1):
-        sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 100)
-        sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 100)
+    while (pins.digital_read_pin(ir1) == 1 and pins.digital_read_pin(ir2) == 0):
+        sensors.dd_mmotor(gb1_direction, 1, gb1_speed, turn_speed)
+        sensors.dd_mmotor(gb2_direction, 1, gb2_speed, turn_speed)
 
     sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 0)
     sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 0)
     
 def adjust_right():
-    while (ir1 == 1 and ir2 == 0):
-        sensors.dd_mmotor(gb1_direction, 1, gb1_speed, 100)
-        sensors.dd_mmotor(gb2_direction, 1, gb2_speed, 100)
+    while (pins.digital_read_pin(ir1) == 0 and pins.digital_read_pin(ir2) == 1):
+        sensors.dd_mmotor(gb1_direction, 0, gb1_speed, turn_speed)
+        sensors.dd_mmotor(gb2_direction, 0, gb2_speed, turn_speed)
 
     sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 0)
     sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 0)
 
 
 def forward(time):
-    sensors.dd_mmotor(gb1_direction, 1, gb1_speed, 100)
-    sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 100)
+    sensors.dd_mmotor(gb1_direction, 0, gb1_speed, speed)
+    sensors.dd_mmotor(gb2_direction, 1, gb2_speed, speed)
     basic.pause(time)
 
+    # Car is angled too far to the right
+    if (pins.digital_read_pin(ir1) == 1 and pins.digital_read_pin(ir2) == 0):
+        stop()
+        adjust_left()
+
+    # Car is angled too far to the left
+    if (pins.digital_read_pin(ir1) == 0 and pins.digital_read_pin(ir2) == 1):
+        stop()
+        adjust_right()
+
 def back(time):
-    sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 100)
-    sensors.dd_mmotor(gb2_direction, 1, gb2_speed, 100)
+    sensors.dd_mmotor(gb1_direction, 1, gb1_speed, speed)
+    sensors.dd_mmotor(gb2_direction, 0, gb2_speed, speed)
     basic.pause(time)
+
+    # Car is angled too far to the right
+    if (pins.digital_read_pin(ir1) == 1 and pins.digital_read_pin(ir2) == 0):
+        stop()
+        adjust_left()
+
+    # Car is angled too far to the left
+    if (pins.digital_read_pin(ir1) == 0 and pins.digital_read_pin(ir2) == 1):
+        stop()
+        adjust_right()
 
 def stop():    
     sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 0)
@@ -71,65 +95,77 @@ def stop():
 
 # For each intersection
 def first():
+    forward(1200)
     stop()
     turn_left()
-    forward(4000)
 
-    stop()
-    back(6000)
+    basic.pause(2000)
 
     forward(2000)
+
+    stop()
+    back(3000)
+
+    forward(1000)
 
     stop()
 
     turn_right()
 
 def second():
+    forward(100)
     stop()
     turn_left()
-    forward(5000)
+    forward(2500)
 
 
     stop()
-    back(5000)
+    back(2500)
 
     turn_right()
 
 def third():
+    forward(100)
     stop()
     turn_left()
-    forward(4000)
 
-    stop()
-    back(8000)
 
     forward(2000)
+
+    stop()
+    back(4000)
+
+    forward(1000)
 
     stop()
 
     turn_right()
 
 def fourth():
+    forward(100)
     stop()
     turn_left()
-    back(2000)
+    back(1000)
 
     stop()
-    forward(7000)
+    forward(3500)
 
     stop()
 
 # Pins
-force = DigitalPin.P0
+force = DigitalPin.P20
 
 ir1 = DigitalPin.P1
-ir2 = DigitalPin.P2
+ir2 = DigitalPin.P8
 
-gb1_speed = AnalogPin.P13
-gb1_direction = AnalogPin.P14
+gb1_speed = AnalogPin.P14
+gb1_direction = AnalogPin.P13
 
-gb2_speed = AnalogPin.P15
-gb2_direction = AnalogPin.P16
+gb2_speed = AnalogPin.P16
+gb2_direction = AnalogPin.P15
+
+speed = 75
+turn_speed = 200
 
 active = False
 
@@ -137,26 +173,16 @@ active = False
 intersectionCount = 0
 
 while True:
-    # Active the car when the force sensor is pressed
-    if (force == 0 and not active):
+
+    if (pins.digital_read_pin(force) == 0 and not active):
         active = True
 
     if (active):
         # Move forward a little bit
         forward(100)
 
-        # Car is angled too far to the right
-        if (ir1 == 0 and ir2 == 1):
-            stop()
-            adjust_left()
-
-        # Car is angled too far to the left
-        if (ir1 == 1 and ir2 == 0):
-            stop()
-            adjust_right()
-
         # If both sensors detect black then it is an intersection
-        if (ir1 == 0 and ir2 == 0):
+        if (pins.digital_read_pin(ir1) == 1 and pins.digital_read_pin(ir2) == 1):
             intersectionCount += 1
 
             if (intersectionCount == 1):
--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
+pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
 //  Utility
 //  Turn left 90 degrees
 function turn_left() {
     let counter = 0
-    sensors.DDMmotor(gb1_direction, 0, gb1_speed, 100)
-    sensors.DDMmotor(gb2_direction, 0, gb2_speed, 100)
     while (true) {
-        if (counter >= 2 && ir1 == 1) {
-            sensors.DDMmotor(gb1_direction, 0, gb1_speed, 0)
-            sensors.DDMmotor(gb2_direction, 0, gb2_speed, 0)
+        basic.showNumber(counter)
+        sensors.DDMmotor(gb1_direction, 1, gb1_speed, turn_speed)
+        sensors.DDMmotor(gb2_direction, 1, gb2_speed, turn_speed)
+        if (counter >= 2 && pins.digitalReadPin(ir1) == 0) {
+            sensors.DDMmotor(gb1_direction, 1, gb1_speed, 0)
+            sensors.DDMmotor(gb2_direction, 1, gb2_speed, 0)
             break
         }
         
-        if (counter == 0 && ir1 == 0) {
+        if (counter == 0 && pins.digitalReadPin(ir1) == 0) {
             counter = 1
-        } else if (counter == 1 && ir1 == 1) {
+        }
+        
+        if (counter == 1 && pins.digitalReadPin(ir1) == 1) {
             counter = 2
         }
         
@@ -23,18 +27,21 @@ function turn_left() {
 //  Turn right 90 degrees
 function turn_right() {
     let counter = 0
-    sensors.DDMmotor(gb1_direction, 1, gb1_speed, 100)
-    sensors.DDMmotor(gb2_direction, 1, gb2_speed, 100)
     while (true) {
-        if (counter >= 2 && ir2 == 1) {
-            sensors.DDMmotor(gb1_direction, 0, gb1_speed, 0)
-            sensors.DDMmotor(gb2_direction, 0, gb2_speed, 0)
+        basic.showNumber(counter)
+        sensors.DDMmotor(gb1_direction, 0, gb1_speed, turn_speed)
+        sensors.DDMmotor(gb2_direction, 0, gb2_speed, turn_speed)
+        if (counter >= 2 && pins.digitalReadPin(ir2) == 0) {
+            sensors.DDMmotor(gb1_direction, 1, gb1_speed, 0)
+            sensors.DDMmotor(gb2_direction, 1, gb2_speed, 0)
             break
         }
         
-        if (counter == 0 && ir2 == 0) {
+        if (counter == 0 && pins.digitalReadPin(ir2) == 0) {
             counter = 1
-        } else if (counter == 1 && ir2 == 1) {
+        }
+        
+        if (counter == 1 && pins.digitalReadPin(ir2) == 1) {
             counter = 2
         }
         
@@ -43,33 +50,57 @@ function turn_right() {
 
 //  Adjust angle so that the car is straight
 function adjust_left() {
-    while (ir1 == 0 && ir2 == 1) {
-        sensors.DDMmotor(gb1_direction, 0, gb1_speed, 100)
-        sensors.DDMmotor(gb2_direction, 0, gb2_speed, 100)
+    while (pins.digitalReadPin(ir1) == 1 && pins.digitalReadPin(ir2) == 0) {
+        sensors.DDMmotor(gb1_direction, 1, gb1_speed, turn_speed)
+        sensors.DDMmotor(gb2_direction, 1, gb2_speed, turn_speed)
     }
     sensors.DDMmotor(gb1_direction, 0, gb1_speed, 0)
     sensors.DDMmotor(gb2_direction, 0, gb2_speed, 0)
 }
 
 function adjust_right() {
-    while (ir1 == 1 && ir2 == 0) {
-        sensors.DDMmotor(gb1_direction, 1, gb1_speed, 100)
-        sensors.DDMmotor(gb2_direction, 1, gb2_speed, 100)
+    while (pins.digitalReadPin(ir1) == 0 && pins.digitalReadPin(ir2) == 1) {
+        sensors.DDMmotor(gb1_direction, 0, gb1_speed, turn_speed)
+        sensors.DDMmotor(gb2_direction, 0, gb2_speed, turn_speed)
     }
     sensors.DDMmotor(gb1_direction, 0, gb1_speed, 0)
     sensors.DDMmotor(gb2_direction, 0, gb2_speed, 0)
 }
 
 function forward(time: number) {
-    sensors.DDMmotor(gb1_direction, 1, gb1_speed, 100)
-    sensors.DDMmotor(gb2_direction, 0, gb2_speed, 100)
+    sensors.DDMmotor(gb1_direction, 0, gb1_speed, speed)
+    sensors.DDMmotor(gb2_direction, 1, gb2_speed, speed)
     basic.pause(time)
+    //  Car is angled too far to the right
+    if (pins.digitalReadPin(ir1) == 1 && pins.digitalReadPin(ir2) == 0) {
+        stop()
+        adjust_left()
+    }
+    
+    //  Car is angled too far to the left
+    if (pins.digitalReadPin(ir1) == 0 && pins.digitalReadPin(ir2) == 1) {
+        stop()
+        adjust_right()
+    }
+    
 }
 
 function back(time: number) {
-    sensors.DDMmotor(gb1_direction, 0, gb1_speed, 100)
-    sensors.DDMmotor(gb2_direction, 1, gb2_speed, 100)
+    sensors.DDMmotor(gb1_direction, 1, gb1_speed, speed)
+    sensors.DDMmotor(gb2_direction, 0, gb2_speed, speed)
     basic.pause(time)
+    //  Car is angled too far to the right
+    if (pins.digitalReadPin(ir1) == 1 && pins.digitalReadPin(ir2) == 0) {
+        stop()
+        adjust_left()
+    }
+    
+    //  Car is angled too far to the left
+    if (pins.digitalReadPin(ir1) == 0 && pins.digitalReadPin(ir2) == 1) {
+        stop()
+        adjust_right()
+    }
+    
 }
 
 function stop() {
@@ -79,79 +110,73 @@ function stop() {
 
 //  For each intersection
 function first() {
+    forward(1200)
     stop()
     turn_left()
-    forward(4000)
-    stop()
-    back(6000)
+    basic.pause(2000)
     forward(2000)
+    stop()
+    back(3000)
+    forward(1000)
     stop()
     turn_right()
 }
 
 function second() {
+    forward(100)
     stop()
     turn_left()
-    forward(5000)
+    forward(2500)
     stop()
-    back(5000)
+    back(2500)
     turn_right()
 }
 
 function third() {
+    forward(100)
     stop()
     turn_left()
-    forward(4000)
-    stop()
-    back(8000)
     forward(2000)
+    stop()
+    back(4000)
+    forward(1000)
     stop()
     turn_right()
 }
 
 function fourth() {
+    forward(100)
     stop()
     turn_left()
-    back(2000)
+    back(1000)
     stop()
-    forward(7000)
+    forward(3500)
     stop()
 }
 
 //  Pins
-let force = DigitalPin.P0
+let force = DigitalPin.P20
 let ir1 = DigitalPin.P1
-let ir2 = DigitalPin.P2
-let gb1_speed = AnalogPin.P13
-let gb1_direction = AnalogPin.P14
-let gb2_speed = AnalogPin.P15
-let gb2_direction = AnalogPin.P16
+let ir2 = DigitalPin.P8
+let gb1_speed = AnalogPin.P14
+let gb1_direction = AnalogPin.P13
+let gb2_speed = AnalogPin.P16
+let gb2_direction = AnalogPin.P15
+let speed = 75
+let turn_speed = 200
 let active = false
 //  Keep track of how many intersections the car has passed
 let intersectionCount = 0
 while (true) {
-    //  Active the car when the force sensor is pressed
-    if (force == 0 && !active) {
+    if (pins.digitalReadPin(force) == 0 && !active) {
         active = true
     }
     
     if (active) {
         //  Move forward a little bit
         forward(100)
-        //  Car is angled too far to the right
-        if (ir1 == 0 && ir2 == 1) {
-            stop()
-            adjust_left()
-        }
-        
-        //  Car is angled too far to the left
-        if (ir1 == 1 && ir2 == 0) {
-            stop()
-            adjust_right()
-        }
-        
         //  If both sensors detect black then it is an intersection
-        if (ir1 == 0 && ir2 == 0) {
+        if (pins.digitalReadPin(ir1) == 1 && pins.digitalReadPin(ir2) == 1) {
             intersectionCount += 1
             if (intersectionCount == 1) {
                 first()
